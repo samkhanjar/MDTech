2dc97c99037911b8cfea40f910f3fe60e73f3e18
Sam Khanjar <sam.khanjar@saiglobal.com>
2018-06-12 08:33:32 +1000

    Creating the API

---
+++ MDTechTest/MDTechTest/Controllers/patient_groupsController.cs
@@ -0,0 +1,30 @@
+﻿using MDTechTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MDTechTest.Controllers
+{
+    public class patient_groupsController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public patient_groupsController(ILogger<patient_groupsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        [Route("api/[controller]/calculate")]
+        public IActionResult Calculate([FromBody] PatientModel patients)
+        {
+            _logger.LogInformation("Calculating number of groups");
+            
+            return Ok();
+        }
+    }
+}
